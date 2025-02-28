447bbf9fb2798d1630a74d787f8ba33aa0af42d6
Krzysztof Łobaziewicz <64072364+klobaziewicz@users.noreply.github.com>
2025-02-28 23:34:35 +0100

    szkielet z dockerem

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Optionally add your own styles
+import App from './App'; // Assuming App.js exists in your src folder
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://backend:8000",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+};
